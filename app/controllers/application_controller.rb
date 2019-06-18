4b89f8aff720cea7ee985e68893df1d4dfc298a4
Anthony Rurup <tcrurup@gmail.com>
2019-06-18 21:29:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
+    if(params[:username])
     user = User.new(:username => params[:username], :password => params[:password])
     if user.save
       redirect '/login'
